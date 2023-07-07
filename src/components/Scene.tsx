dbf0cbc15b8a01b72fb1b35f80eabb35806ac6d6
Bryon <bryon.sigawin@gmail.com>
2023-07-07 19:25:43 +0800

    feat: render basic scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,7 +1,15 @@
+import { Canvas } from '@react-three/fiber';
+
 export default function Scene() {
 	return (
-	<div>
-		Example text
-	</div>
+		<div className="absolute w-screen h-screen t-0 l-0">
+			<Canvas>
+				<pointLight position={[10, 10, 10]} />
+				<mesh>
+					<sphereGeometry />
+					<meshStandardMaterial color="hotpink" />
+				</mesh>
+			</Canvas>
+		</div>
 	)
 }
