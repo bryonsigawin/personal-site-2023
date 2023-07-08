89a2370111cd2c81049c4b6d38df3478272cadbf
Bryon <bryon.sigawin@gmail.com>
2023-07-08 13:06:31 +0800

    feat: prettify files

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,15 +1,15 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas } from "@react-three/fiber";
 
 export default function Scene() {
-	return (
-		<div className="absolute w-screen h-screen t-0 l-0">
-			<Canvas>
-				<pointLight position={[10, 10, 10]} />
-				<mesh>
-					<sphereGeometry />
-					<meshStandardMaterial color="hotpink" />
-				</mesh>
-			</Canvas>
-		</div>
-	)
+  return (
+    <div className="absolute w-screen h-screen t-0 l-0">
+      <Canvas>
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <sphereGeometry />
+          StandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
