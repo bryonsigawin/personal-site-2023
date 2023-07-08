45df116131c4a80ba61fb61eade36137a3655322
Bryon <bryon.sigawin@gmail.com>
2023-07-08 18:07:39 +0800

    feat: setup isometric camera

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,15 +1,26 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrthographicCamera } from "@react-three/drei";
 
 export default function Scene() {
   return (
     <div className="absolute w-screen h-screen t-0 l-0">
       <Canvas>
         <pointLight position={[10, 10, 10]} />
-        <mesh>
-          <sphereGeometry />
-          StandardMaterial color="hotpink" />
+        <Camera />
+        <mesh position={[0, 1, 0]}>
+          <boxGeometry args={[8, 0.5, 8]} />
+          <meshStandardMaterial color="white" />
         </mesh>
       </Canvas>
     </div>
   );
+}
+
+function Camera() {
+  useFrame((state) => {
+    state.camera.lookAt(0, 0, 0);
+    state.camera.updateProjectionMatrix();
+  });
+
+  return <OrthographicCamera makeDefault position={[5, 5, 5]} zoom={40} />;
 }
